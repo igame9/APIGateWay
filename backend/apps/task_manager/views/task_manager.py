87237a92f0bd81a289e627252606755e39ea0aef
Andrey <andreygrigorev0@gmail.com>
2024-08-04 13:42:29 +0300

    add AllowAny to TaskManagerProxy

--- backend/apps/task_manager/views/task_manager.py
+++ backend/apps/task_manager/views/task_manager.py
@@ -1,20 +1,23 @@
 import requests
 from django.http import HttpResponse
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
 
 TASK_MANAGER_SERVICE_URL = 'http://localhost:8000/'  # !!!
 
 
 class TaskManagerProxy(APIView):
+    permission_classes = [AllowAny]
+
     def dispatch(self, request, *args, **kwargs):
         method = request.method
         path = kwargs.get('path', '')
 
-        url = f'{TASK_MANAGER_SERVICE_URL}/{path}'
-        print(url)
+        url = f'{TASK_MANAGER_SERVICE_URL}{path}'
 
         headers = {key: value for key, value in request.headers.items() if key.lower() != 'host'}
+
         data = request.body
 
         response = requests.request(method, url, headers=headers, data=data, params=request.GET)
